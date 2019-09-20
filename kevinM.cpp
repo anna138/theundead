b1858a92abfbdabec34e4ae747448ca5259efd60
Kevin Mitra <kmitra@odin.cs.csubak.edu>
2019-09-20 11:05:23 -0700

    Make changes to credits

--- kevinM.cpp
+++ kevinM.cpp
@@ -1,2 +1,11 @@
 // Author: Kevin Mitra OpenAL
 // CMPS 3350
+// created on: 9/20/2019
+//
+//
+#include "fonts.h"
+
+void creditsKevin(Rect r)
+{
+    ggprint8b(&r, 16, 0x00ff0000, "Kevin Mitra");
+}
