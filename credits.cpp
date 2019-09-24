4defe8b0f76d2038f3b7862fecb9f37419f01e43
Manvir <mbal2@csub.edu>
2019-09-24 12:06:59 -0700

    added 3d mode to credits

--- credits.cpp
+++ credits.cpp
@@ -3,6 +3,7 @@
 
 #include "fonts.h"
 #include <GL/glx.h>
+extern void chaos_equations();
 
 void showCredits(Rect r)
 {
@@ -14,60 +15,14 @@ void showCredits(Rect r)
     ggprint8b(&r, 16, 0x00ff0000, "Anna Poon");
 }
 
-float t = -3.0f;
-float inc = 0.005;
-double xp = t; 
-double yp = t;
 void renderCoolCredits(){
 	glMatrixMode(GL_PROJECTION); glLoadIdentity();
 	glMatrixMode(GL_MODELVIEW); glLoadIdentity();
-	
-    glOrtho(-4,4,-4,4,-1,1);
-    //glFrustum(-4,4,-4,4,1.0,10);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glPushMatrix();
-		glEnable(GL_POINT_SMOOTH);
-		//glColor3f(255,255,255);
+    //glOrtho(-4,4,-4,4,-1,1);
+    glFrustum(-4,4,-4,4,1.0,10);
 
-		//glVertex2i(0,0);
-		//float t = -3.0f;
-		double xp = t; 
-		double yp = t;
-		double x = xp; 
-		double y = yp;
 
-		for (int i =  0; i < 1000;i++){
-			// x = x_r[i];
-			// y = y_r[i];
-			xp = x*x - x*t + y + t;
-			yp = x*x + y*y + t*t - x*t - x + y;
-			x = xp;
-			y = yp;
-			//std::cout << "this is x: " << xp << " and this is yp:" << yp << std::endl;
-			//sleep(1);
-			if(i%1){
-				glPointSize(5);
-				//glColor3ub(rand()%255,rand()%255,rand()%255);
-				glColor3ub(255,255,255);
-			}
-			else{
-				glPointSize(5);
-				glColor3ub(255,255,255);
-			}
-			glBegin(GL_POINTS);
-
-			glVertex3d(xp,yp, -1.0);
-			glEnd();
-
-		}
-	if(t >= 1.0f){
-		inc = -inc;
-	}else if(t <= -1.0f){
-		inc = 0.005;
-	}
-	t += inc;
-
-	glPopMatrix();
+    chaos_equations();
 
 
 }
--- manvirB.cpp
+++ manvirB.cpp
@@ -12,4 +12,46 @@ void creditManvir(Rect r)
 {
     ggprint8b(&r, 16, 0x00ff0000, "Manvir Bal");
 }
+float t = 0.05f;
+float inc = 0.00005;
+double xp = t; 
+double yp = t;
+int angle = 5;
+void chaos_equations(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+		//glRotatef(angle, 0,0,1);
+		glScalef(10,6,1);
+		glEnable(GL_POINT_SMOOTH);
+		xp = t;
+		yp = t;
+		double x = xp; 
+		double y = yp;
 
+		for (int i =  0; i < 1000;i++){
+			xp = x*x - x*t + y + t;
+			yp = x*x + y*y + t*t - x*t - x + y;
+			x = xp;
+			y = yp;
+			// std::cout << "this is x: " << xp << " and this is yp:" << yp << std::endl;
+			// sleep(1);
+
+			glPointSize(5);
+			//glColor3ub(rand()%255,rand()%255,rand()%255);
+			glColor3ub(255,255,255);
+			glBegin(GL_POINTS);
+				//glVertex2d(xp,yp);
+				glVertex3d(xp,yp, -1.1);
+			glEnd();
+
+		}
+	if(t >= 0.05f){
+		inc = -inc;
+	}else if(t <= -0.05f){
+		inc = 0.00005;
+	}
+	t += inc;
+	angle = (angle + 1)%360;
+
+	glPopMatrix();
+}
