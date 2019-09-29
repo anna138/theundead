561238cdf2abb9c2d58ef2609e0dcc44421b179e
Anna Poon <42920422+anna138@users.noreply.github.com>
2019-09-29 08:59:15 -0700

    fixed spacing

--- annaP.cpp
+++ annaP.cpp
@@ -35,11 +35,11 @@ void startMenu(Rect r, int y, int x, int img_x, int img_y, GLuint imageTexture)
 	img_x = img_y + imageTexture;
 	img_y = 0 + img_x;
 	imageTexture =0;
-    r.bot = 0-(y/3);
-    r.left = 0-(x/7);
-    r.center = 0;
-    ggprint16(&r, 16, 0x00ff0000, "Press Space to Continue");
-    ggprint16(&r, 16, 0x00ff0000, 
+    	r.bot = 0-(y/3);
+    	r.left = 0-(x/7);
+    	r.center = 0;
+    	ggprint16(&r, 16, 0x00ff0000, "Press Space to Continue");
+    	ggprint16(&r, 16, 0x00ff0000, 
             "Press Space + C for Credits During Gameplay");
 }
 
@@ -48,11 +48,11 @@ void displayLogo(int w, int h, int x, int y, GLuint texture)
 	
 	int width = w/2;
 	int height = h/2;
-    glPushMatrix();
+    	glPushMatrix();
 	//glTranslatef(,0,0);
-    glColor3f(1.0,1.0,1.0);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glBegin(GL_QUADS);
+    	glColor3f(1.0,1.0,1.0);
+    	glBindTexture(GL_TEXTURE_2D, texture);
+    	glBegin(GL_QUADS);
         glTexCoord2f(0, 0);
         glVertex2i(-width,-height);
         glTexCoord2f(0, 1);
@@ -61,14 +61,14 @@ void displayLogo(int w, int h, int x, int y, GLuint texture)
         glVertex2i(width, height);
         glTexCoord2f(1,0);
         glVertex2i(width,-height);
-    glEnd();
-    glPopMatrix();
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glDisable(GL_ALPHA_TEST);
+    	glEnd();
+    	glPopMatrix();
+    	glBindTexture(GL_TEXTURE_2D, 0);
+    	glDisable(GL_ALPHA_TEST);
     
 }
 
 void creditsAnna(Rect r)
 {
-    ggprint8b(&r, 16, 0x00004C00, "Anna Poon");
+    	ggprint8b(&r, 16, 0x00004C00, "Anna Poon");
 }
